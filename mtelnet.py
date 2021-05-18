eed86f30d5d4c4a5da88d2d8012f5e81694a1fc4
mahdi <mahdi.rezaie.336@gmail.com>
2021-05-18 06:05:51 +0430

    Remove unnecessary fields from mtelnet

--- mtelnet.py
+++ mtelnet.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python3
-import os
 import traceback
 
 from Invoker import Invoker
-from client import Client
-from server import Server
-from command import *
 import sys
 from socket import socket, SOCK_STREAM, AF_INET
 
 
 class MTelnet:
-
-    client_commands = ['upload', 'exec', 'send']
-    server_commands = ['listen']
-    other_commands = ['history', 'help']
 
     def __init__(self):
         self.socket = socket(AF_INET, SOCK_STREAM)
@@ -28,17 +20,14 @@ class MTelnet:
         try:
             while True:
                 command = input('mtelnet> ').split()
-                if command[0] in MTelnet.client_commands or \
-                        command[0] in MTelnet.server_commands or \
-                        command[0] in MTelnet.other_commands:
+                try:
+                    r = self.command_factory.execute(command[0], command[1:])
+                    print(r)
+                except Exception as e:
+                    print('error')
 
-                    try:
-                        r = self.command_factory.execute(command[0], command[1:])
-                        print(r)
-                    except Exception as e:
-                        print('error')
         except KeyboardInterrupt:
-            traceback.print_exc('Program terminated by kill signal.')
+            print('\n\nProgram is terminating due to a keyboard interrupt.')
             self.socket.close()
 
 
