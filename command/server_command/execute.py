e6dd34d30c2c7f928821bdcfe1f4c05a5156da5d
mahdi <mahdi.rezaie.336@gmail.com>
2021-05-25 17:58:07 +0430

    Define server command execute

---
+++ command/server_command/execute.py
@@ -0,0 +1,13 @@
+import os
+
+from command import Command
+from msocket import MSocket
+
+
+class Execute(Command):
+
+    def execute(self, socket: MSocket, args: list[str]) -> str:
+        return os.popen(' '.join(args)).read()
+
+    def help(self) -> str:
+        return 'Executes a command into the connected node.\n\n'
