7dd55820cc46d9ffcdae5623eb0b84f5da4b2d6e
mahdi <mahdi.rezaie.336@gmail.com>
2021-05-27 14:13:43 +0430

    Create history command

---
+++ command/history.py
@@ -0,0 +1,20 @@
+from command import Command
+from msocket import MSocket
+from shared_resources import SharedResources
+from dbmanager import DBManager
+
+
+class History(Command):
+
+    def execute(self, socket: MSocket, args: list[str]) -> str:
+        result = ''
+        res = SharedResources()
+        if 'db' not in res:
+            db = res.add_attribute('db', DBManager())
+        else:
+            db = res.get_attribute('db')
+        result += db.read_all()
+        return result
+
+    def help(self) -> str:
+        return 'Prints a history of all command which you have input.\n'
